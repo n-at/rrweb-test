d4dd7580bb3ac0e7086df5e54bff3877ed1f7f36
Alexey Nurgaliev <atnurgaliev@gmail.com>
2021-07-19 11:01:51 +0300

    bootstrap 5 content

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'rrweb/dist/rrweb'
 
 import { createApp } from 'vue'
 
